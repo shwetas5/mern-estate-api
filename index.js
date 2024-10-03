a6c817ad82f24f9f1fcb88f31c6ae427264b7182
swamy <thippeswamy35@gmail.com>
2024-10-03 16:53:27 +0530

    routing

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import uerRouter from './routes/user.route.js'
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -9,8 +11,12 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log('DB Error :',err)
 })
 
+
+
 const app= express();
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000!!!')
 })
+
+app.use('/api/user', userRouter)

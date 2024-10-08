d5f30e906e5b4e6526aae0804a4123a320eee64d
swamy <thippeswamy35@gmail.com>
2024-10-08 10:28:11 +0530

    error middleware

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 
-
 const app= express();
 app.use(express.json())
 
@@ -23,3 +22,12 @@ app.listen(3000, ()=>{
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+app.use((err, req, res, next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})

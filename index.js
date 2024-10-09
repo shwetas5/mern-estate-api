f0d7e1a74a6f9b3681b229fe1207a2a328aa074f
swamy <thippeswamy35@gmail.com>
2024-10-09 23:48:53 +0530

    update user listing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import uerRouter from './routes/user.route.js'
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
@@ -24,6 +25,9 @@ app.listen(3000, ()=>{
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/listing', listingRouter)
+
+
 app.use((err, req, res, next) =>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal server error';
